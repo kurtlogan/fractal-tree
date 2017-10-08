3cbeb2292baaa9d09f2d98d574a29a2092a94d8c
Kurt Logan <kurt.logan@digital.hmrc.gov.uk>
2017-10-08 12:36:20 +0100

    Added stroke weights

--- sketch.js
+++ sketch.js
@@ -3,6 +3,8 @@ var startSize = 100
 var startAngle = -90
 var distanceMult = 0.7
 var angleDiff = 25
+var startWeight = 10
+var weightDiff = 1
 
 var startSizeMax = 200
 
@@ -15,11 +17,15 @@ var distanceMultMax = 0.9
 var angleDiffMin = -360
 var angleDiffMax = 360
 
+var startWeightMax = 50
+
+var weightDiffMax = 50
+
 function setup() {
   createCanvas(windowWidth, windowHeight)
 
   gui = createGui('Controls')
-  gui.addGlobals('startSize', 'startAngle', 'distanceMult', 'angleDiff')
+  gui.addGlobals('startSize', 'startAngle', 'distanceMult', 'angleDiff', 'startWeight', 'weightDiff')
 
   noLoop()
 }
@@ -28,16 +34,18 @@ function draw() {
   clear()
   background(0)
   stroke(255)
-  drawLine(width / 2, height, startAngle, startSize)
+  drawLine(width / 2, height, startAngle, startSize, startWeight)
 }
 
-function drawLine(x, y, angle, distance) {
+function drawLine(x, y, angle, distance, weight) {
   let newPoint = calculateNewPoint(x, y, angle, distance)
+
+  strokeWeight(weight)
   line(x, y, newPoint.x, newPoint.y)
 
-  if(distance > 2) {
-    drawLine(newPoint.x, newPoint.y, angle - angleDiff, distance * distanceMult)
-    drawLine(newPoint.x, newPoint.y, angle + angleDiff, distance * distanceMult)
+  if(distance > 5) {
+    drawLine(newPoint.x, newPoint.y, angle - angleDiff, distance * distanceMult, weight - weightDiff)
+    drawLine(newPoint.x, newPoint.y, angle + angleDiff, distance * distanceMult, weight - weightDiff)
   }
 }
 
